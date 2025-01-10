0d5f9a578eaaa42a520c99a71403e7838afd37c6
TheoLSu23c <154343177+TheoLSu23c@users.noreply.github.com>
2025-01-10 09:21:36 +0100

    PowerUp Dubbel Damage

--- Assets/PowerUp.cs
+++ Assets/PowerUp.cs
@@ -1,10 +1,12 @@
+using System;
+
 public class Player
 {
     public int DefultDamage { get; set; }
     public bool ÄrDubbelDamagePå { get; private set;}
     private float DubbelSkadasDuration;
     private float DubbelSkadasTid;
-
+    public bool SamladePowerUp;
     public Player(int DefultDamage)
     {
         DefultDamage = DefultDamage;
@@ -61,14 +63,53 @@ public class Player
     public class Game
     {
         private Player player;
-        private PowerUp puwerUp;
+        private PowerUp powerUp;
 
         public Game()
         {
+            player = new Player(20); //Normal skadan.
+            powerUp = new PowerUp();
+
+        }
+
+        public void Update(float deltaTime)
+        {
+            player.Update(deltaTime);
+
+            if (player.SamladePowerUp)
+            {
+                powerUp.Activate(player);
+            }
 
 
         }
 
+        private bool KontroleraOmPowerUpHarSamlat()
+        {
+
+
+            return false;
+        }
+
+        public void PlayerAtack()
+        {
+            int DamageGjort = player.GörSkada();
+            Console.WriteLine($"Player gör{DamageGjort}Skada");
+        }
+
+
     }
 
+    public static void Main(string[] args)
+    {
+        Game game = new Game();
+
+        float deltaTime = 0.1f; 
+        for (int i = 0; i < 60; i++) 
+        {
+            game.Update(deltaTime);
+            game.PlayerAtack();
+        }
+    }
+    //check
 }
