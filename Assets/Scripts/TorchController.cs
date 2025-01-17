ccf2845cb1b56f84925b82a606fc2ba24a36ec4a
Skoliboi <elias.weise@elev.ga.lbs.se>
2025-01-17 13:48:15 +0100

    :3

--- Assets/Scripts/TorchController.cs
+++ Assets/Scripts/TorchController.cs
@@ -4,17 +4,22 @@ public class Torch : MonoBehaviour
 {
     public int torchIndex; // Assign a unique index to each torch
     private TorchManager TorchPuzzle;
+    private AudioSource audioSource;
+    //public AudioClip RightoTorch;
 
     private void Start()
     {
         TorchPuzzle = FindObjectOfType<TorchManager>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void OnMouseDown()
     {
         // Change sprite to "lit" state
+        audioSource.Play();
         GetComponent<SpriteRenderer>().sprite = TorchPuzzle.litTorchSprite;
         // Notify the puzzle manager
         TorchPuzzle.LightTorch(torchIndex);
+        
     }
 }
